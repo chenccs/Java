5d009fff39572e6b5c1adbd7114722c151b955f3
Administrator <2416762675@qq.com>
2017-01-03 18:21:41 +0800

    toCool
    yes

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Cool!");
     }
+    public static void  toCool() {
+        System.out.println("Cool!");
+    }
 }
