8de790114959109e7ba152cbb040ac8e7b199b5a
Administrator <2416762675@qq.com>
2017-01-03 18:22:48 +0800

    cool

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,7 @@ public class Main {
     public static void  toCool() {
         System.out.println("Cool!");
     }
+    public static void  toOtherCool() {
+        System.out.println("Cool!");
+    }
 }
